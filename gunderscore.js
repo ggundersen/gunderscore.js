c005e040ddefad52e190528610a355f78f1bdf28
Gregory Gundersen <ggundersen@gmail.com>
2013-10-21 21:08:13 -0400

    added each, reduce, isExisential, isTruthy, and identity

--- gunderscore.js
+++ gunderscore.js
@@ -2,35 +2,83 @@
  * gunderscore.js
  * ----------------------------------------------------------------*/
 
- (function() {
+ var g_ = (function() {
 
- 	/* Test data. No function should ever mutate this variable.
+ 	/* `gunderscore` object
  	 */
- 	var items = [1, 2, 3];
+	var g_ = {};
+
+	/* Iterators
+	 *-------------------------------------------------------------*/
 
 	/* `each` is an immutable iterator. It is the quintessential
 	 * example of a functional style. Note that its implementation uses a
 	 * loop. Functional programming does not eliminate imperative
 	 * concepts; rather, it abstracts them away with a function. Any
-	 * loss in performance can be mitigated or regained by an
-	 * optimizer.
+	 * loss in performance can be mitigated or regained by a
+	 * compressor.
 	 */
-	var each = function(iter, fn) {
-		// what is the difference between `each` and `map`?
+	var each = g_.each = function(iterable, fn) {
+		var i, len;
+
+		for (i = 0, len = iterable.length; i < len; i++) {
+			fn(i);
+		}
 	};
 
 	/* `map` applies a supplied function to every element of an array
-	 * and returns a new array
+	 * and returns a new array. Notice how it can use `each`; we are
+	 * quickly building upon small abstractions.
 	 */
-	var map = function(iter, fn) {
-		var i,
-			result = [];
+	var map = g_.map = function(iterable, fn) {
+		var result = [];
 
-		for (i = 0; i < iter.length; i++) {
-			result.push( fn(iter[i]) );
-		}
-
+		each(iterable, function(i) {
+			result.push( fn(iterable[i]) );
+		});
+		
 		return result;
+	};
+
+	/*
+	 */
+	var reduce = g_.reduce = function(iterable) {
+		each(iterable, function)
 	}
+
+	/* Utility
+	 *-------------------------------------------------------------*/
+
+	/* `identity` returns the value it is passed. This abstraction is
+	 * surprisingly important because, since functional programming
+	 * focuses on functions rather than values (for configuration),
+	 * we often need to pass in `identity`.
+	 */
+	g_.identity = function(value) {
+		return value;
+	}
+
+	/* Predicates
+	 *-------------------------------------------------------------*/
+
+	/* `isExistential` is a boolean function that returns whether an
+	 * element exists (is neither `undefined` nor `null`). Loose
+	 * equality makes this a one-liner.
+	 */
+	g_.isExistential = function(el) {
+		return el != null;
+	};
+
+	/* `isTruthy` defines
+	 */
+	g_.isTruthy = function(el) {
+		return (el !== false) && g_.isExistential(el);
+	};
+
+	g_.isLessOrEqual = function(x, y) {
+		return x <= x;
+	};
+
+	return g_;
 
  })();
