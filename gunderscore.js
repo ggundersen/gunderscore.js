ee8578f6aaf15b9c465d967abdc11f5359c4676c
Gregory Gundersen <ggundersen@gmail.com>
2013-10-21 22:23:17 -0400

    reduce is complete

--- gunderscore.js
+++ gunderscore.js
@@ -8,46 +8,71 @@
  	 */
 	var g_ = {};
 
-	/* Iterators
-	 *-------------------------------------------------------------*/
+/* Collection functions
+ *
+ * @Fogus: 'Functional programming is extremely useful for tasks
+ * requiring that some operation happen on many items in a
+ * collection... The point of a collection-centric view... is to a
+ * consistent processing idiom so that we can reuse a comprehensive
+ * set of functions.'
+ * ----------------------------------------------------------------*/
 
 	/* `each` is an immutable iterator. It is the quintessential
-	 * example of a functional style. Note that its implementation uses a
-	 * loop. Functional programming does not eliminate imperative
-	 * concepts; rather, it abstracts them away with a function. Any
-	 * loss in performance can be mitigated or regained by a
-	 * compressor.
+	 * example of a functional style. Note that its implementation
+	 * uses a `for` loop. Functional programming does not eliminate
+	 * imperative concepts; rather, it abstracts them away with a
+	 * function. Any loss in performance can be mitigated or
+	 * regained by a compressor.
 	 */
-	var each = g_.each = function(iterable, fn) {
+	var each = g_.each = function(collection, func) {
 		var i, len;
 
-		for (i = 0, len = iterable.length; i < len; i++) {
-			fn(i);
+		for (i = 0, len = collection.length; i < len; i++) {
+			func(i);
 		}
 	};
 
-	/* `map` applies a supplied function to every element of an array
-	 * and returns a new array. Notice how it can use `each`; we are
-	 * quickly building upon small abstractions.
+	/* `map` calls a function on every value in a collection,
+	 * returning a collection of results. Notice how it can use
+	 * `each`; we are quickly building upon small abstractions.
 	 */
-	var map = g_.map = function(iterable, fn) {
+	var map = g_.map = function(collection, func) {
 		var result = [];
 
-		each(iterable, function(i) {
-			result.push( fn(iterable[i]) );
+		each(collection, function(i) {
+			result.push( func(collection[i]) );
 		});
 		
 		return result;
 	};
 
-	/*
+	/* `reduce` (or `fold`) calls a function on every value in a
+	 * collection, accumulates the result, and returns it.
 	 */
-	var reduce = g_.reduce = function(iterable) {
-		each(iterable, function)
-	}
+	var reduce = g_.reduce = function(collection, func) {
+		var result = 0;
 
-	/* Utility
-	 *-------------------------------------------------------------*/
+		each(collection, function(i) {
+			result += func(collection[i]);
+		});
+
+		return result;
+	};
+
+	/* `filter` calls a predicate function on each item in a
+	 * collection, returning a collection of predicates
+	 */
+	var filter = g_.filter = function(collection, predicate) {
+		var result;
+
+	};
+
+	g_.join = function(collection) {
+		// ?
+	};
+
+/* Utility
+ * ----------------------------------------------------------------*/
 
 	/* `identity` returns the value it is passed. This abstraction is
 	 * surprisingly important because, since functional programming
@@ -58,27 +83,27 @@
 		return value;
 	}
 
-	/* Predicates
-	 *-------------------------------------------------------------*/
+/* Predicates
+ *
+ * @Fogus: 'Functions that always return a Boolean value are called
+ * "predicates."'
+ * ----------------------------------------------------------------*/
 
 	/* `isExistential` is a boolean function that returns whether an
 	 * element exists (is neither `undefined` nor `null`). Loose
 	 * equality makes this a one-liner.
 	 */
-	g_.isExistential = function(el) {
-		return el != null;
+	g_.isExistential = function(value) {
+		return value != null;
 	};
 
-	/* `isTruthy` defines
-	 */
-	g_.isTruthy = function(el) {
-		return (el !== false) && g_.isExistential(el);
-	};
-
-	g_.isLessOrEqual = function(x, y) {
-		return x <= x;
+	g_.isTruthy = function(value) {
+		return (value !== false) && g_.isExistential(value);
 	};
 
 	return g_;
 
  })();
+
+ var items = [1, 2, 3];
+ var double = function(x) { return x*x; };
